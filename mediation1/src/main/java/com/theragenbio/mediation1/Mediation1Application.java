d9aa8f4cef5007a4875ee42dba49a90b70c62da9
Joon Park <joonhyuck.park@theragenbio.com>
2022-02-26 17:02:28 +0900

    first commit

--- mediation1/src/main/java/com/theragenbio/mediation1/Mediation1Application.java
+++ mediation1/src/main/java/com/theragenbio/mediation1/Mediation1Application.java
@@ -2,10 +2,12 @@ package com.theragenbio.mediation1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class Mediation1Application {
 
 	public static void main(String[] args) {
--- serviceB/src/main/java/com/theragenbio/serviceB/ServiceBApplication.java
+++ serviceB/src/main/java/com/theragenbio/serviceB/ServiceBApplication.java
@@ -2,10 +2,12 @@ package com.theragenbio.serviceB;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class ServiceBApplication {
 
 	public static void main(String[] args) {
