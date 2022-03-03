9da6ff3b1ebe0e6f46bd2f83c8bf69917917defc
Joon Park <joonhyuck.park@theragenbio.com>
2022-03-03 17:30:03 +0900

    commit 220303_3

--- mediation1/src/main/java/com/theragenbio/mediation1/interfaces/ServiceBInterface.java
+++ mediation1/src/main/java/com/theragenbio/mediation1/interfaces/ServiceBInterface.java
@@ -1,5 +1,14 @@
 package com.theragenbio.mediation1.interfaces;
 
-public class ServiceBInterface {
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+@FeignClient(name = "serviceB")
+public interface ServiceBInterface {
+    @GetMapping("/dept/")
+    String getAllDepartments() throws Exception;
+
+	@GetMapping("/dept/{deptId}")
+	String findByDepartmentId(@PathVariable("deptId") int userId) throws Exception;
 }
--- mediation2/src/main/java/com/theragenbio/mediation2/controller/MediationController.java
+++ mediation2/src/main/java/com/theragenbio/mediation2/controller/MediationController.java
@@ -5,11 +5,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.theragenbio.mediation2.interfaces.ServiceAInterface;
+import com.theragenbio.mediation2.interfaces.ServiceBInterface;
 
 @RestController
 public class MediationController {
 	@Autowired
 	ServiceAInterface serviceA;
+
+
+	@Autowired
+	ServiceBInterface serviceB;
 
 	@GetMapping(path = "/")
 	public String getAllUsers() throws Exception
@@ -19,4 +24,12 @@ public class MediationController {
 		
 	}
 
+	@GetMapping(path = "/svc")
+	public String getAllDepartments() throws Exception
+	{
+		System.out.println("SERVICE 2 --SETTING--");
+		return serviceB.getAllDepartments();
+		
+	}
+	
 }
